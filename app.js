df6895225eb7705b1b6e51dc23d6d7488e66eea6
emran71 <ekhan4475@gmail.com>
2025-05-07 14:19:30 +0600

    Comoleted the form data shows html element with tabular format

---
+++ app.js
@@ -0,0 +1,47 @@
+const users = JSON.parse(localStorage.getItem('usersdata')) || [];
+console.log(users);
+
+function displayUsers() {
+      tbody.innerHTML = ''; // Clear table body
+      users.forEach((user, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${index + 1}</td>
+          <td>${user.username}</td>
+          <td>${user.name}</td>
+          <td>${user.email}</td>
+          <td>${user.address}</td>
+          <td>${user.mobile}</td>
+        `;
+        tbody.appendChild(row);
+      });
+  }
+
+const formdata = document.querySelector("#userForm");
+console.log(formdata);
+const tbody = document.querySelector('#table-part tbody');
+//console.log(tbody);
+formdata.addEventListener('submit', function(event) {
+
+    event.preventDefault(); 
+
+   let user ={
+        username : document.getElementById('username').value,
+        name : document.getElementById('name').value,
+        email : document.getElementById('email').value,    
+        address : document.getElementById('address').value,   
+        mobile : document.getElementById('mobile').value
+   }
+
+    if (user.username && user.name && user.email && user.address && user.mobile) {
+        let localData = JSON.parse(localStorage.getItem('usersdata')) || [];
+        localData.push(user);
+        localStorage.setItem('usersdata', JSON.stringify(localData));
+        location.reload();
+        displayUsers()
+        formdata.reset();
+    } else {
+        alert("Please fill all fields");
+    }
+});
+displayUsers();
